c1b8668908e05dc04641458c0026b7ac95026ae7
DripMicro <whappydrip@gmail.com>
2022-09-24 00:52:39 -0600

    Modify slice

--- helloworld.go
+++ helloworld.go
@@ -5,14 +5,37 @@ import (
 )
 
 func main() {
-	myslice1 := make([]int, 5, 10)
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
 	fmt.Printf("myslice1 = %v\n", myslice1)
 	fmt.Printf("length = %d\n", len(myslice1))
 	fmt.Printf("capacity = %d\n", cap(myslice1))
 
-	// with omitted capacity
-	myslice2 := make([]int, 5)
-	fmt.Printf("myslice2 = %v\n", myslice2)
-	fmt.Printf("length = %d\n", len(myslice2))
-	fmt.Printf("capacity = %d\n", cap(myslice2))
+	myslice1 = append(myslice1, 20, 21)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = []int{1, 2, 3}
+	myslice2 := []int{4, 5}
+	myslice3 := append(myslice1, myslice2...)
+
+	fmt.Printf("myslice3=%v\n", myslice3)
+	fmt.Printf("length=%d\n", len(myslice3))
+	fmt.Printf("capacity=%d\n", cap(myslice3))
+
+	arr1 := [6]int{9, 10, 11, 12, 13, 14} // An array
+	myslice1 = arr1[1:5]                  // Slice array
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = arr1[1:3] // Change length by re-slicing the array
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21, 22, 23) // Change length by appending items
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 }
