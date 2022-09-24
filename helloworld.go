e956904dfd408027480fbb4664e8f9565e514560
DripMicro <whappydrip@gmail.com>
2022-09-23 19:14:20 -0600

    arrays

--- helloworld.go
+++ helloworld.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	var txt1 string = "Hello!"
-	var txt2 string
-	txt3 := "World 1"
+	arr1 := [5]int{1: 10, 2: 40}
+	arr2 := [5]int{1, 2}          //partially initialized
+	arr3 := [5]int{1, 2, 3, 4, 5} //fully initialized
+	arr4 := [...]int{1, 2, 3, 4, 5, 6}
 
-	fmt.Printf("Type: %T, value: %v\n", txt1, txt1)
-	fmt.Printf("Type: %T, value: %v\n", txt2, txt2)
-	fmt.Printf("Type: %T, value: %v\n", txt3, txt3)
-
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+	fmt.Println(len(arr4))
 }
