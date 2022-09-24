9aca20e44c50669c0b3908564a95928814e5baa1
DripMicro <whappydrip@gmail.com>
2022-09-23 18:15:34 -0600

    Declear Multiple Variables2

--- helloworld.go
+++ helloworld.go
@@ -5,11 +5,13 @@ import (
 )
 
 func main() {
-	var a, b = 6, "Hello"
-	c, d := 1, "World!"
+	var (
+		a int
+		b int    = 1
+		c string = "hello"
+	)
 
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Println(d)
 }
