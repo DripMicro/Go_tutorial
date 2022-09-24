0cbfc45f378f1df2b4f0e9dca1a95aafb2a8cac6
DripMicro <whappydrip@gmail.com>
2022-09-23 19:03:58 -0600

    Boolean

--- helloworld.go
+++ helloworld.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	var a bool = true
-	var b int = 5
-	var c float32 = 3.14
-	var d string = "hi"
+	var b1 bool = true // typed declaration with initial value
+	var b2 = true      // untyped declaration with initial value
+	var b3 bool        // typed declaration without initial value
+	b4 := true         // untyped declaration with initial value
 
-	fmt.Println("Boolean: ", a)
-	fmt.Println("Integer: ", b)
-	fmt.Println("Float: ", c)
-	fmt.Println("String: ", d)
+	fmt.Println(b1) // Returns true
+	fmt.Println(b2) // Returns true
+	fmt.Println(b3) // Returns false
+	fmt.Println(b4) // Returns true
 }
