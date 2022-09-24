3a57b004b1a089b2f89ae68d253727bd9c727657
DripMicro <whappydrip@gmail.com>
2022-09-23 18:27:37 -0600

    Constants1

--- helloworld.go
+++ helloworld.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 )
 
-const PI = 3.14
+const (
+	A int = 1
+	B     = 3.14
+	C     = "Hi!"
+)
 
 func main() {
-	fmt.Println(PI)
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
 }
