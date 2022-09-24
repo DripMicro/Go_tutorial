622b66e09b4e06624e85603bf5783e7f4f1a15bd
DripMicro <whappydrip@gmail.com>
2022-09-24 00:43:20 -0600

    create slice

--- helloworld.go
+++ helloworld.go
@@ -5,10 +5,14 @@ import (
 )
 
 func main() {
-	arr1 := [6]int{10, 11, 12, 13, 14, 15}
-	myslice := arr1[2:4]
+	myslice1 := make([]int, 5, 10)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 
-	fmt.Printf("myslice = %v\n", myslice)
-	fmt.Printf("length = %d\n", len(myslice))
-	fmt.Printf("capacity = %d\n", cap(myslice))
+	// with omitted capacity
+	myslice2 := make([]int, 5)
+	fmt.Printf("myslice2 = %v\n", myslice2)
+	fmt.Printf("length = %d\n", len(myslice2))
+	fmt.Printf("capacity = %d\n", cap(myslice2))
 }
